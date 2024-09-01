30c7ccfa3f7389bb3b3d305fb1dbf859216dc322
Frankline Were <franklinegift@gmail.com>
2024-09-01 21:14:57 +0300

    list comprehension: even numbers

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
+
+print(return_evens((1, 2, 3, 4, 5, 6, 7)))
 
 def make_exclamation(sentence_list):
     pass
