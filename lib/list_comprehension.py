687405c442de7458331337b6079bba65a71503bd
Frankline Were <franklinegift@gmail.com>
2024-09-01 21:21:52 +0300

    List Comprehension: Add Exclamation mark

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -3,7 +3,10 @@
 def return_evens(num_list):
     return [num for num in num_list if num % 2 == 0]
 
-print(return_evens((1, 2, 3, 4, 5, 6, 7)))
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
+    
